a6bbdff4b21789d4e63340f7092bd06092efcfe8
KEVIN BAZIRA <kvnbzr@gmail.com>
2019-03-11 16:28:38 +0300

    Added: '' or 'col-sm-2' class left sidebar

---
+++ sidebar-left.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Algori_Shop
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ):
+?>
+	<aside id="secondary" class="widget-area <?php echo ( class_exists( 'WooCommerce' ) && is_woocommerce() ) ? '' : 'col-sm-2' ; ?> sidebar">
+	</aside><!-- #secondary -->
+<?php endif; ?>
