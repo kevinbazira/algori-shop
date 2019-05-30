74b093227c3ab0bca2c62cfd294b263a6237763a
KEVIN BAZIRA <kvnbzr@gmail.com>
2019-05-30 12:15:46 +0300

    Added: documentation link to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -25,6 +25,46 @@ function algori_shop_customize_register( $wp_customize ) {
 			'render_callback' => 'algori_shop_customize_partial_blogdescription',
 		) );
 	}
+	
+	/**
+	* Algori Shop Custom Customizer Customizations [ PS: The order matters ( add_panel, add_section, add_setting, add_control ) ]
+	*/
+	
+	/**
+	 * Create custom control for Algori Shop Documentation link.
+	 *
+	 * Class WP_Customize_Control is loaded only when theme customizer is acutally used. 
+	 * So, I've defined my custom class within the algori_shop_customize_register function that is binded to the 'customize_register' action.
+	 *
+	 * @link https://developer.wordpress.org/themes/customize-api/customizer-objects/#custom-controls-sections-and-panels.
+	 */ 
+	class WP_Documentation_Link_Customize_Control extends WP_Customize_Control {
+		public $type = 'algori_shop_documentation_link';
+		/** 
+		* Render the control's content. esc_url($my_theme->get( 'AuthorURI' ))
+		*/
+		public function render_content() {
+		?>
+			<a href="<?php echo esc_url( wp_get_theme()->get( 'AuthorURI' ) ) . 'blog/article/algori-shop-gutenberg-ready-woocommerce-wordpress-theme'; ?>" class="button button-primary" id="algori-shop-documentation-link" target="_blank" tabindex="0"><?php _e( 'Documentation', 'algori-shop' ); ?></a>
+		<?php
+		}
+	}
+	
+	// Documentation Section
+	$wp_customize->add_section( 'algori_shop_documentation', array(
+		'title'				=> __( 'Algori Shop Documentation', 'algori-shop' ),
+		'description'		=> __( 'View Algori Shop Documentation.', 'algori-shop' ),
+		'priority'			=> 1,
+		'capability'		=> 'edit_theme_options',
+	    'theme_supports' 	=> '', // Rarely needed.
+	) );
+	
+	// Documentation Button ( from custom control WP_Documentation_Link_Customize_Control )
+	$wp_customize->add_control( new WP_Documentation_Link_Customize_Control( $wp_customize, 'documentation_button', array(
+        'section'			=> 'algori_shop_documentation',
+        'settings'			=> array(),
+    ) ) );
+	
 }
 add_action( 'customize_register', 'algori_shop_customize_register' );
 
@@ -53,3 +93,11 @@ function algori_shop_customize_preview_js() {
 	wp_enqueue_script( 'algori-shop-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'algori_shop_customize_preview_js' );
+
+/**
+ * Enqueue CSS for styling custom controls in the customizer.
+ */
+function algori_shop_customize_preview_assets() {
+	wp_enqueue_style( 'algori-shop-customizer-custom-controls', get_template_directory_uri() . '/style/css/customizer.css', array(), '20190530', 'all' );
+}
+add_action( 'customize_controls_enqueue_scripts', 'algori_shop_customize_preview_assets' );
